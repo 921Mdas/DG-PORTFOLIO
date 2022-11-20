870f18ba94dd8d44ce14e124114a16429d4b6f5e
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-11-20 11:46:33 -0500

    blobeffectcomplete

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -1,8 +1,9 @@
 import TextScrambler from "react-scramble-text";
 import "react-scramble-text/dist/index.css";
-import { useThree } from "react-three-fiber";
+import { useFrame, useThree } from "react-three-fiber";
 import { useEffect } from "react";
 import gsap from "gsap";
+import { useRef } from "react";
 
 export const Scrambler = ({ phrases, speed, pauseTime }) => {
   return (
@@ -27,29 +28,57 @@ export const MovePageNeg = (page, perc) => {
 };
 
 export const R3FCameraAnimatedEvents = () => {
-  const width = window.innerWidth;
   const height = window.innerHeight;
   const { camera } = useThree();
-  const tl = gsap.timeline();
+
+  window.addEventListener("scroll", e => {
+    const scrollRatio = window.scrollY / height;
+    camera.position.y = -scrollRatio * 1.5;
+  });
 
   // scroll and move animation base
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      const scrollRatio = window.scrollY / height;
-      camera.position.y = -scrollRatio * 3;
-    });
 
-    window.addEventListener("mousemove", e => {
-      if (!e) return;
-      const { clientX } = e;
-      let ratioWidth = clientX / width;
+  // window.addEventListener("scroll", () => {
+  //   const scrollRatio = window.scrollY / height;
+  //   // tl.to(camera.position, {
+  //   //   duration: 1,
+  //   // });
+  //   // camera.position.y = -scrollRatio * 3;
+  // });
 
-      camera.position.x += (ratioWidth - camera.position.x) * 0.01;
-    });
+  // useFrame((state, delta) => {
+  //   window.addEventListener("mousemove", e => {
+  //     if (!e) return;
+  //     const { clientX, clientY } = e;
+  //     const ratioX = clientX / width;
+  //     const ratioY = clientY / height;
 
-    return () => {
-      window.removeEventListener("scroll", window);
-      window.removeEventListener("mousemove", window);
-    };
-  }, [camera.position, height, width, tl]);
+  //     camera.position.x += (ratioX - camera.position.x) * 0.1 * delta;
+  //     camera.position.y += (ratioY - camera.position.y) * 0.1 * delta;
+  //   });
+  // });
+
+  // useEffect(() => {
+  //   window.addEventListener("mousemove", e => {
+  //     if (!e) return;
+  //     const { clientX, clientY } = e;
+  //     const ratioX = Math.cos(clientX / window.innerWidth);
+  //     const ratioY = Math.sin(clientY / window.innerHeight);
+
+  //     camera.position.x += Math.sin(ratioX / 0.1);
+  //     camera.position.y += Math.sin(ratioY / 0.1);
+  //   });
+
+  //   return () => {
+  //     window.removeEventListener("scroll", window);
+  //     window.removeEventListener("mousemove", window);
+  //   };
+  // }, [camera.position, height, width, tl]);
+
+  //  window.addEventListener(
+  //   "scroll",
+  //   e => {
+  //     const ratio = window.scrollY / window.innerHeight;
+  //     camera.position.y = -ratio * 5;
+  //   });
 };
