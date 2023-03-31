57318e947ab534f1dd5077fe07e3d57eb69d3759
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-03-30 20:25:20 -0400

    noisy

--- src/Components/threejs/Util/ParagraphHelper.tsx
+++ src/Components/threejs/Util/ParagraphHelper.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, forwardRef } from "react";
 import { Text, useScroll } from "@react-three/drei";
 import * as THREE from "three";
 import { Vector3 } from "three";
 import Rubik from "../../../assets/Fonts/RubikBold.ttf";
 import { useThree } from "react-three-fiber";
 import { useFrame } from "react-three-fiber";
+import { vertexShader, fragmentShader } from "./Shader";
+import SimulationMaterial from "../Util/simulationMaterial";
+
 interface Paragraph {
   text: string;
   color: string;
@@ -17,6 +20,7 @@ interface Paragraph {
   fnOver?: () => any;
   fnOut?: () => any;
   fnClick?: () => any;
+  shade: boolean;
 }
 
 const ParagraphHelper: React.FC<Paragraph> = ({
@@ -31,34 +35,83 @@ const ParagraphHelper: React.FC<Paragraph> = ({
   fnOver,
   fnOut,
   fnClick,
+  shade = false,
 }) => {
   const data = useScroll();
   const { width, height } = useThree(state => state.viewport);
   const grp = useRef();
+  const geomRef = useRef();
 
-  useFrame(() => {
-    // if (grp.current) grp.current.scale.z = 1 + data.range(0, 1 / 3) / 3;
-    // console.log(grp.current);
-    // data.offset
+  function getPoint(v, size, data, offset) {
+    v.set(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+    if (v.length() > 1) return getPoint(v, size, data, offset);
+    return v.normalize().multiplyScalar(size).toArray(data, offset);
+  }
+
+  function getSphere(count, size, p = new THREE.Vector4()) {
+    const data = new Float32Array(count * 4);
+    for (let i = 0; i < count * 4; i += 4) getPoint(p, size, data, i);
+    return data;
+  }
+
+  const positionsTexture = new THREE.DataTexture(
+    getSphere(512 * 512, 128),
+    512,
+    512,
+    THREE.RGBAFormat,
+    THREE.FloatType
+  );
+  positionsTexture.needsUpdate = true;
+
+  useEffect(() => {
+    const count = geomRef.current.geometry.attributes.position.count;
+    const randoms = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+      randoms[i] = Math.random();
+    }
+    geomRef.current.geometry.setAttribute(
+      "aRandom",
+      new THREE.BufferAttribute(randoms, 1)
+    );
+  }, []);
+
+  useFrame((state, delta) => {
+    shaderMat.uniforms.uTime.value += delta;
   });
-  const material = new THREE.MeshStandardMaterial({
-    color: "red",
-    toneMapped: true,
-    blending: THREE.AdditiveBlending,
+
+  const shaderMat = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {
+      uTime: { value: 0 },
+      positions: { value: 0 },
+      uCurlFreq: { value: 0.25 },
+    },
   });
+
+  const mat = shade
+    ? shaderMat
+    : new THREE.MeshStandardMaterial({
+        color: "red",
+        toneMapped: true,
+        blending: THREE.AdditiveBlending,
+        roughness: 0,
+        metalness: 0.2,
+      });
 
   return (
     <Text
-      color={color}
       scale={scale}
       lineHeight={lineHeight}
       anchorX={anchorX}
       anchorY={anchorY}
-      material={material}
       position={position}
       font={font}
       onClick={fnClick}
       ref={grp}
+      shade={shade}
+      ref={geomRef}
     >
       {text}
     </Text>
@@ -66,3 +119,6 @@ const ParagraphHelper: React.FC<Paragraph> = ({
 };
 
 export default ParagraphHelper;
+function getSphere(arg0: number, arg1: number): BufferSource {
+  throw new Error("Function not implemented.");
+}
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,18 +1,25 @@
+import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
-const isCodeSandbox = !!process.env.SANDBOX_URL;
+import curlNoise from "glsl-curl-noise2";
+import classicNoise from "glsl-noise/classic/3d.glsl";
 
-export default {
-  root: "src/",
-  publicDir: "../static/",
-  base: ".",
-  server: {
-    host: true,
-    open: !isCodeSandbox,
-  },
-
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-};
+export default defineConfig({
+  plugins: [
+    glsl({
+      include: [/\.glsl$/, /\.vert$/, /\.frag$/],
+      transform: (code, id) => {
+        if (/\.glsl$/.test(id)) {
+          code = code.replace(
+            /#pragma glslify: curl = require\('glsl-curl-noise2'\)/g,
+            curlNoise
+          );
+          code = code.replace(
+            /#pragma glslify: noise = require\('glsl-noise\/classic\/3d.glsl'\)/g,
+            classicNoise
+          );
+        }
+        return code;
+      },
+    }),
+  ],
+});
